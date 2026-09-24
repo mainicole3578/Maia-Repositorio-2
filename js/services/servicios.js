07e3d4650f9037a7929b77b136780f1d79ede45b
Maia Campi <maiacampi3578@gmail.com>
2026-09-23 22:18:19 -0300

    se agregaron cambios en el html y en el css

--- js/services/servicios.js
+++ js/services/servicios.js
@@ -11,3 +11,23 @@ export const obtenerPokemon = async (pokemon) => {
 
     return datos;
 };
+export const obtenerPokemones = async () => {
+    const respuesta = await fetch(`${URL_API}?limit=12`);
+
+    if (!respuesta.ok) {
+        throw new Error("No se pudieron obtener los Pokémon");
+    }
+   const datos = await respuesta.json();
+
+    const pokemones = [];
+
+    for (const pokemon of datos.results) {
+        const respuestaPokemon = await fetch(pokemon.url);
+        const datosPokemon = await respuestaPokemon.json();
+
+        pokemones.push(datosPokemon);
+    }
+
+    return pokemones;
+};
+
