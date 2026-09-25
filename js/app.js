ca3d6c76e4ddfe8d5665fdf3edf7024901c5e2be
Maia Campi <maiacampi3578@gmail.com>
2026-09-25 00:18:38 -0300

    Se agrego la carga de Pokemon desde la API

---
+++ js/app.js
@@ -0,0 +1,62 @@
+import { obtenerPokemones } from "./services/servicios.js";
+import { crearTarjetaPokemon } from "./components/pokemonCard.js";
+
+const formulario = document.getElementById("formularioBusqueda");
+const busqueda = document.getElementById("busqueda");
+const contenedorPokemon = document.getElementById("contenedorPokemon");
+const btnVolver = document.getElementById("btnVolver");
+
+const spinner = document.getElementById("spinner");
+const textoCarga = document.getElementById("textoCarga")
+
+const mostrarCarga = () => {spinner.classList.remove("d-none");
+textoCarga.classList.remove("d-none");
+};
+
+const ocultarCarga = () => {
+    spinner.classList.add ("d-none");
+    textoCarga.classList.add ("d.none");
+};
+
+const cargaPokemones = async () => {
+    mostrarCarga();
+
+    try{ const pokemones = await obtenerPokemones();
+        contenedorPokemon.innerHTML = " ";
+        pokemones.forEach((pokemon) => {
+            contenedorPokemon.innerHTML += crearTarjetaPokemon(pokemon);
+        });
+
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "No se pudieron cargar los pokemon."
+
+    });
+    return;
+    }
+
+    mostrarCarga();
+    try{ const pokemon = await obtenerPokemon(nombrePokemon) ;
+        contenedorPokemon.innerHTML  = crearTarjetaPokemon(pokemon);
+    } catch (error) {
+        contenedorPokemon.innerHTML = " ";
+
+        Swal.fire({
+            icon: "error",
+            title: "Pokémon no encontrado",
+            text: "No encontramos ese Pokémon.",
+            confirmButtonText: "Volver a la lista"
+        }).then(()  => {
+            cargarPokemones();
+        });
+    } finally {
+        ocultarCarga();
+}
+btnVolver.addEventListener("click", () => {
+    busqueda.value = "";
+    cargarPokemones();
+});
+};
+cargarPokemones();
